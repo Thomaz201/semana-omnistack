f17f2956630f230eed96adebb1d1cafc1e09e950
Thomaz201 <lucasthomazr@hotmail.com>
2020-03-25 21:46:50 -0300

    Front-end do App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './global.css';
+
+import Routes from './routes';
+
+/**
+ * Quando o HTML está escrito dentro do código JS nós o chamamos de JSX(JavaScript XML)
+ */
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Lucas Thomaz!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes />
     </div>
   );
 }
